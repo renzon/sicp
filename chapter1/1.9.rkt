2db6d99c0acbd821b66c51f1f343bf8fd7807c86
renzon <renzon@gmail.com>
2016-10-28 16:40:40 -0200

    ex 1.9

---
+++ chapter1/1.9.rkt
@@ -0,0 +1,76 @@
+#lang racket
+
+(define (+ a b)
+  (if (= a 0)
+      b
+      (inc (+ (dec a) b))))
+
+; aplying substituion on after eah new line usins above + procedure
+
+(+ 4 5)
+
+(if (= 4 0)
+    b
+    (inc (+ (dec a) b))))
+
+
+(inc (+ (dec 4) 5))))
+
+
+
+(inc (+ 3 5))))
+
+
+(inc
+ (if (= 3 0)
+     b
+     (inc (+ (dec a) b)))))
+
+
+(inc (inc (+ (dec 3) 5)))))
+     
+
+(inc (inc (+ 2 5)))))
+     
+
+(inc (inc (inc (+ 1 5))))
+
+(inc (inc (inc (inc (+ 0 5)))))
+
+(inc (inc (inc (inc 5))))
+
+(inc (inc (inc 6)))
+
+(inc (inc 7))
+
+(inc 8)
+
+9
+
+; so this is a recursive process, next another definition
+(define (+ a b)
+  (if (= a 0)
+      b
+      (+ (dec a) (inc b))))
+
+; aplying substituion on after eah new line usins above + procedure
+
+(+ 4 5)
+
+(if (= 4 0)
+    b
+    (+ (dec a) (inc b)))
+
+(+ (dec 4) (inc 5))
+
+(+ 3 6)
+
+(+ 2 7)
+
+(+ 1 8)
+
+(+ 0 9)
+
+9
+
+;it is a linear process

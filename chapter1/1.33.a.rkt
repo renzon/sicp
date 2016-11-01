28ec76fa3abe6ed9e072a4509f7081f6ffa46391
renzon <renzon@gmail.com>
2016-11-01 21:15:25 -0200

    ex 1.33

---
+++ chapter1/1.33.a.rkt
@@ -0,0 +1,39 @@
+#lang racket
+(require test-engine/racket-tests)
+
+
+(define (filtered-accumulate combiner null-value term a next b filter)
+  (define (iter a result)
+    (cond ((> a b) result)
+          ((filter a) (iter (next a) (combiner (term a) result)))
+          (else (iter (next a) result))))
+  (iter a null-value))
+
+
+(define (square n) (* n n))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (prime? n) (if (< n 2) #f (fast-prime? n 100)))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m)) m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m)) m))))
+(define (inc x) (+ x 1))
+
+
+(check-expect 38 (filtered-accumulate + 0 square 1 inc 5 prime?))
+(check-expect 87 (filtered-accumulate + 0 square 1 inc 7 prime?))
+
+(test)

03ddae35cf5bf4258e77f95bbbcebcbc83c88362
renzon <renzon@gmail.com>
2016-11-06 12:33:13 -0200

    ex 2.29 b

--- chapter2/2.29.rkt
+++ chapter2/2.29.rkt
@@ -16,11 +16,21 @@
 
 (define (branch-structure branch) (car (cdr branch)))
 
+(define (total-weight mobile)
+  (define (branch-weight structure)
+    (cond ((null? structure) 0)
+          ((pair? structure) (branch-weight (branch-structure structure)))
+          (else structure)))
+    
+  (+ (branch-weight (left-branch mobile)) (branch-weight (right-branch mobile))))
+        
 (define l-branch (make-branch 1 2))
 (define r-branch (make-branch 3 4))
-
 (define mobile (make-mobile l-branch r-branch))
+
 (check-expect (branch-length (left-branch mobile)) 1)
 (check-expect (branch-structure (right-branch mobile)) 4)
+(check-expect (total-weight mobile) 6)
+(check-expect (total-weight(make-mobile l-branch l-branch)) 4)
 
 (test)

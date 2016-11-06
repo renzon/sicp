bd6fde05df3a9f3fdb18d22b936116d5b5727f7e
renzon <renzon@gmail.com>
2016-11-06 13:47:55 -0200

    ex 2.29 b fix for coumpound mobile

--- chapter2/2.29.rkt
+++ chapter2/2.29.rkt
@@ -17,20 +17,26 @@
 (define (branch-structure branch) (car (cdr branch)))
 
 (define (total-weight mobile)
-  (define (branch-weight structure)
-    (cond ((null? structure) 0)
-          ((pair? structure) (branch-weight (branch-structure structure)))
-          (else structure)))
-    
-  (+ (branch-weight (left-branch mobile)) (branch-weight (right-branch mobile))))
+  (define (branch-weight branch)
+    (total-weight (branch-structure branch)))
+  (if (pair? mobile)  
+      (+ (branch-weight (left-branch mobile)) (branch-weight (right-branch mobile)))
+      mobile))
+  
         
 (define l-branch (make-branch 1 2))
 (define r-branch (make-branch 3 4))
 (define mobile (make-mobile l-branch r-branch))
+(define double-two-mobile (make-mobile l-branch l-branch))
+
+(define coumpound-branch (make-branch 3 double-two-mobile))
+
+(define coumpound-mobile (make-mobile coumpound-branch r-branch))
 
 (check-expect (branch-length (left-branch mobile)) 1)
 (check-expect (branch-structure (right-branch mobile)) 4)
 (check-expect (total-weight mobile) 6)
-(check-expect (total-weight(make-mobile l-branch l-branch)) 4)
+(check-expect (total-weight double-two-mobile) 4)
+(check-expect (total-weight coumpound-mobile) 8)
 
 (test)

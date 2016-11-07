4f29f71a140a65b195689d158699a836f57ee7e6
renzon <renzon@gmail.com>
2016-11-06 22:18:45 -0200

    ex 2.29 d, only right-branch and branch-structure changed,
    it can be checked on diff with previous commit

--- chapter2/2.29.rkt
+++ chapter2/2.29.rkt
@@ -2,19 +2,18 @@
 (require test-engine/racket-tests)
 
 (define (make-mobile left right)
-  (list left right))
+  (cons left right))
 
 (define (make-branch length structure)
-  (list length structure))
+  (cons length structure))
 
 (define (left-branch mobile) (car mobile))
 
-(define (right-branch mobile) (car (cdr mobile)))
-
+(define (right-branch mobile) (cdr mobile))
 
 (define (branch-length branch) (car branch))
 
-(define (branch-structure branch) (car (cdr branch)))
+(define (branch-structure branch) (cdr branch))
 
 (define (total-weight mobile)
   (define (branch-weight branch)
@@ -22,6 +21,21 @@
   (if (pair? mobile)  
       (+ (branch-weight (left-branch mobile)) (branch-weight (right-branch mobile)))
       mobile))
+
+(define (balanced? mobile)
+  (define (torque branch)
+    (* (branch-length branch) (total-weight (branch-structure branch))))
+  (define (balanced-branch? branch)
+    (balanced? (branch-structure branch)))
+  
+  (if (pair? mobile)
+      (let ((left (left-branch mobile))
+            (right (right-branch mobile)))
+        (and
+         (balanced-branch? left)
+         (balanced-branch? right)
+         (= (torque left) (torque right))))
+      #t))
   
         
 (define l-branch (make-branch 1 2))
@@ -30,13 +44,19 @@
 (define double-two-mobile (make-mobile l-branch l-branch))
 
 (define coumpound-branch (make-branch 3 double-two-mobile))
-
 (define coumpound-mobile (make-mobile coumpound-branch r-branch))
 
+(define coumpound-branch-2 (make-branch 4 double-two-mobile))
+(define unbalanced-coumpound-mobile (make-mobile coumpound-branch-2 r-branch))
 (check-expect (branch-length (left-branch mobile)) 1)
 (check-expect (branch-structure (right-branch mobile)) 4)
 (check-expect (total-weight mobile) 6)
 (check-expect (total-weight double-two-mobile) 4)
 (check-expect (total-weight coumpound-mobile) 8)
+
+(check-expect (balanced? mobile) #f)
+(check-expect (balanced? double-two-mobile) #t)
+(check-expect (balanced? coumpound-mobile) #t)
+(check-expect (balanced? unbalanced-coumpound-mobile) #f)
 
 (test)
